38b617169e730c403d19939a48f7f36404b1b685
akash100a <arakash968786@gmail.com>
2024-10-16 18:13:21 +0600

    p

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const appServeer = express();
 appServeer.get('/', (req, res) => {
     res.send("Hellow");
 })
+appServeer.get('/read', (req, res) => {
+    res.send("Hellow Reading");
+})
 
 appServeer.listen(4000,()=>{
     console.log("server is working");
