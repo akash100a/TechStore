b273ab0c638fb82b1a9d16f03a3b53f321272998
akash100a <arakash968786@gmail.com>
2024-10-17 12:39:23 +0600

    a

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let userDetails = {
     "products": [
         {
             "id": 6,
-            "title": "Calvin Klein CK One",
+            "title": "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On",
             "description": "CK One by Calvin Klein is a classic unisex fragrance, known for its fresh and clean scent. It's a versatile fragrance suitable for everyday wear.",
             "category": "fragrances",
             "price": 49.99,
@@ -70,7 +70,7 @@ let userDetails = {
                 "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/2.png",
                 "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png"
             ],
-            "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/thumbnail.png"
+            "thumbnail":"https://akash100a.github.io/TechStore/public/images/pc.jpg"
         },
         {
             "id": 7,
